0a360c5c8212b2797e6c01c56368d842a0d5ded9
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-07-11 08:28:44 +0300

    adding checkNumber function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const clearNumber = () => {
   document.getElementById("results-div").textContent = "";
 };
+
+const checkNumber = (str) => {
+  const regex = /^1? ?(\(\d{3}\) ?|\d{3}[ -]?)\d{3}[ -]?\d{4}$/;
+  return regex.test(str);
+};
