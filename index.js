aa382d0c524880701a61f2c8f21c2cf94489a02b
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-07-11 08:45:48 +0300

    adding event listener to clear button

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const clearNumber = () => {
-  document.getElementById("results-div").textContent = "";
+const clearNumber = (result, input) => {
+  console.log("clicked");
+  result.textContent = "";
+  input.value = "";
 };
 
 const checkNumber = (str) => {
   const regex = /^1? ?(\(\d{3}\) ?|\d{3}[ -]?)\d{3}[ -]?\d{4}$/;
   return regex.test(str);
+};
+
+const printResult = (condition, input, result) => {
+  return condition
+    ? (result.textContent = "Valid US number: " + input)
+    : (result.textContent = "Invalid US number: " + input);
+};
+
+window.onload = () => {
+  const result = document.getElementById("results-div");
+  const input = document.getElementById("user-input");
+
+  document.getElementById("clear-button").addEventListener("click", () => {
+    clearNumber(result, input);
+  });
 };
